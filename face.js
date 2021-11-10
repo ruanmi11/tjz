07f21fe07ead0f4308c2588530023c0a6fab38ad
xiaohua <565724897@qq.com>
2021-11-10 22:41:04 +0800

    开始

--- face.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	var img1=document.getElementById("img1");
-	var face=document.getElementById("face");
-	addEvent(face,"change",function(){
-		img1.src="images/face/"+face.value;
-	})
-})();
